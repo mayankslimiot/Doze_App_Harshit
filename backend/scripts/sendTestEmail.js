9cc518afee8cd115df7ba845bebfad02462b32db
mayankslimiot <mayank.pratap@slimiot.com>
2026-05-03 10:52:14 +0530

    stable version: untrack backend and other unnecessary folders

--- backend/scripts/sendTestEmail.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { sendNewUserCredentials, verifySmtp } = require('../utils/mailer');
-
-(async () => {
-  await verifySmtp();
-  await sendNewUserCredentials('your-test-email@domain.com', 'Test User', 'Temp1234!');
-  console.log('Sent!');
-})();
--- backend/utils/userId.js
+++
@@ -1,7 +0,0 @@
-async function nextUserId(account) {
-  const suffixBase = "abcdefghijklmnopqrstuvwxyz";
-  const used = account.userProfiles?.length || 0;
-  return `${account.accountId}${suffixBase[used]}`;
-}
-
-module.exports = { nextUserId };
